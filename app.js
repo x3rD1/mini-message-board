81b062f68767958bd91a7294d60a9f9a2cc48d5b
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-10-27 01:52:16 -0700

    Add process.env.PORT for railway to provide dynamic port number

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 // Middlewares
 app.use("/", indexRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Initiating Mini message board app");
 });
