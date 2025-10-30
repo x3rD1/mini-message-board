25ec07e3465175a5975a951b87e24fee1b3a7b3c
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-10-30 15:45:28 -0700

    Added messageValidator

---
+++ validators/messageValidator.js
@@ -0,0 +1,20 @@
+const { body } = require("express-validator");
+
+const validateMessage = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Name is required.")
+    .isAlpha()
+    .withMessage("Name must only contain letters.")
+    .isLength({ min: 3, max: 12 })
+    .withMessage("Name must be between 3 and 12 characters."),
+  body("message")
+    .trim()
+    .notEmpty()
+    .withMessage("Message is required.")
+    .isLength({ max: 200 })
+    .withMessage("Message cannot exceed 200 characters"),
+];
+
+module.exports = { validateMessage };
